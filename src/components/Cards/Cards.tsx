db215fbb5d68acfaf9d095283668d8a8fabf8f05
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-05-05 23:11:02 -0300

    .

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,23 +1,23 @@
 import HalfRating from "../Home/HalfRating/HalfRating";
 import { AreaCard, AreaTitleRating } from "./CardsStyle";
 import { CardsProps } from "../../types/common/common";
-import { MoviesResponse, MovieTrailer } from "../../types/movies/movies";
-import { api } from "../../service/api";
-import { useState } from "react";
+// import { MoviesResponse, MovieTrailer } from "../../types/movies/movies";
+// import { api } from "../../service/api";
+// import { useState } from "react";
 
 const Cards = ({ id, poster_path, title, name, vote_average }: CardsProps) => {
-  const [_, setKeyTrailer] = useState<string | undefined>();
+  // const [_, setKeyTrailer] = useState<string | undefined>();
 
-  async function getMovieTrailerKey(movieId: number): Promise<void> {
-    try {
-      const connection = await api.get<MoviesResponse<MovieTrailer[]>>(
-        `/movie/${movieId}/videos`
-      );
-      setKeyTrailer(connection.data.results[0].key);
-    } catch (error) {
-      console.error("Erro ao buscar dados:", error);
-    }
-  }
+  // async function getMovieTrailerKey(movieId: number): Promise<void> {
+  //   try {
+  //     const connection = await api.get<MoviesResponse<MovieTrailer[]>>(
+  //       `/movie/${movieId}/videos`
+  //     );
+  //     setKeyTrailer(connection.data.results[0].key);
+  //   } catch (error) {
+  //     console.error("Erro ao buscar dados:", error);
+  //   }
+  // }
 
   return (
     <AreaCard key={id}>
