aee1e7c2d3a00257730d6e4d79146aa5e2431eb3
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2025-05-06 21:09:32 -0300

    Create router details

--- src/Routes/RoutesApp.tsx
+++ src/Routes/RoutesApp.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Movies from "../pages/Movies/Movies";
 import Series from "../pages/Series/Series";
+import Details from "../pages/Details/Details";
 
 const RoutesApp = () => {
   return (
@@ -10,6 +11,7 @@ const RoutesApp = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path={`/filmes`} element={<Movies />}></Route>
         <Route path={`/series`} element={<Series />}></Route>
+        <Route path={`/details/:id/:title`} element={<Details />}></Route>
         <Route path="/*" element={<p>not found</p>}></Route>
       </Routes>
     </BrowserRouter>
--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -8,44 +8,68 @@ import { CommonContext } from "../../context/Common/CommonContext";
 // import { api } from "../../service/api";
 // import { useState } from "react";
 import noImage from "../../assets/No_Image_Available.jpg";
+import { Link } from "react-router-dom";
 
 const Cards = ({ id, poster_path, title, name, vote_average }: CardsProps) => {
   const { loading } = useContext(CommonContext);
 
-  // const [_, setKeyTrailer] = useState<string | undefined>();
+  // const [keyTrailer, setKeyTrailer] = useState<string | undefined>();
+
+  // useEffect(() => {
+  //   async function getMovieTrailerKey(movieId: number): Promise<void> {
+  //     try {
+  //       const connection = await api.get<MoviesResponse<MovieTrailer[]>>(
+  //         `/movie/${movieId}/videos`,
+  //         {
+  //           params: {
+  //             language: `en-US`,
+  //           },
+  //         }
+  //       );
+  //       console.log(connection);
+  //       setKeyTrailer(connection.data.results[0]?.key);
+  //     } catch (error) {
+  //       console.error("Erro ao buscar dados:", error);
+  //     }
+  //   }
+
+  //   getMovieTrailerKey(id);
+  // });
 
   // async function getMovieTrailerKey(movieId: number): Promise<void> {
   //   try {
   //     const connection = await api.get<MoviesResponse<MovieTrailer[]>>(
   //       `/movie/${movieId}/videos`
   //     );
-  //     setKeyTrailer(connection.data.results[0].key);
+  //     setKeyTrailer(connection.data.results[0]?.key);
   //   } catch (error) {
   //     console.error("Erro ao buscar dados:", error);
   //   }
   // }
 
   return (
-    <AreaCard key={id}>
-      {loading ? (
-        <SkeletonCards />
-      ) : (
-        <section>
-          <img
-            src={
-              !poster_path
-                ? noImage
-                : `https://image.tmdb.org/t/p/original${poster_path}`
-            }
-            alt={`Poster ${title || name}`}
-          />
-          <AreaTitleRating>
-            <p>{title || name}</p>
-            <HalfRating vote={vote_average} />
-          </AreaTitleRating>
-        </section>
-      )}
-    </AreaCard>
+    <Link to={`/details/${id}/${title || name}`}>
+      <AreaCard key={id}>
+        {loading ? (
+          <SkeletonCards />
+        ) : (
+          <section>
+            <img
+              src={
+                !poster_path
+                  ? noImage
+                  : `https://image.tmdb.org/t/p/original${poster_path}`
+              }
+              alt={`Poster ${title || name}`}
+            />
+            <AreaTitleRating>
+              <p>{title || name}</p>
+              <HalfRating vote={vote_average} />
+            </AreaTitleRating>
+          </section>
+        )}
+      </AreaCard>
+    </Link>
   );
 };
 
